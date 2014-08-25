07ad5e197b44b43bdfee8ae564ac466cfdc153c4
Peter <info@jbaron.com>
2014-08-25 10:00:55 +0200

    updated copyright notices
    Where missing, added Apache license at beginning of source files

--- src/cats/gui/sourceEditor.ts
+++ src/cats/gui/sourceEditor.ts
@@ -1,5 +1,19 @@
-    var EditSession: Ace.EditSession = ace.require("ace/edit_session").EditSession;
-    var UndoManager: Ace.UndoManager = ace.require("ace/undomanager").UndoManager;
+// Copyright (c) JBaron.  All rights reserved.
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+var EditSession: Ace.EditSession = ace.require("ace/edit_session").EditSession;
+var UndoManager: Ace.UndoManager = ace.require("ace/undomanager").UndoManager;
 
 /**
  * Wrapper around the ACE editor. The rest of the code base should not use
