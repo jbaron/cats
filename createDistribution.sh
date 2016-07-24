264bd626277c217cc0640521d3e4085f9d0fd575
JBaron <info@jbaron.com>
2016-07-25 00:10:24 +0200

    updated version for creating distributions

--- createDistribution.sh
+++ createDistribution.sh
@@ -1,2 +1,2 @@
 nr=`git log lib/*.js | grep '^commit ' | wc -l | tr -d ' '`
-zip -r "../cats-1.8.${nr}.nw" lib/* resource/* node_modules/* CopyrightNotice.txt LICENSE.txt index.html package.json
+zip -r "../cats-2.0.${nr}.nw" lib/* resource/* node_modules/* CopyrightNotice.txt LICENSE.txt index.html package.json
